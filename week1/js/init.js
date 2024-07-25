1978e67b6a97203324b38d6505ff531e918f6414
sachigill <sachigill@g.ucla.edu>
2024-07-25 15:23:26 -0700

    lab 2

--- week1/js/init.js
+++ week1/js/init.js
@@ -1,7 +1,7 @@
 // Initialize the map
 const map = new maplibregl.Map({
     container: 'map', // container ID
-    style: 'https://api.maptiler.com/maps/streets/style.json?key=wsyYBQjqRwKnNsZrtci1', // Your style URL
+    style: 'https://api.maptiler.com/maps/streets-v2-light/style.json?key=7w3ejiOsCvODMfBB0ggd', // Your style URL
     center: [ -118.444, 34.0709], // Starting position [lng, lat]
     zoom: 15 // Starting zoom level
 });
@@ -16,7 +16,7 @@ new maplibregl.Marker()
 new maplibregl.Marker()
     .setLngLat([ -118.44190, 34.06646])
     .setPopup(new maplibregl.Popup({ offset: 25 }) // Add popups
-        .setHTML('Biomedical Library<br>  '))
+        .setHTML('<div class="lib"> Biomedical Library</tags></div>  '))
     .addTo(map);
 
 new maplibregl.Marker()
